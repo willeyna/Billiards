a70b08561144fe8c57900fea57d214d6b8178731
willeyna <willeyna@msu.edu>
2022-08-22 22:01:51 -0400

    Changed 2D minimization to 1D root finding problem; 10x+ faster

--- billiards.py
+++ billiards.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize, basinhopping
+from scipy.optimize import brentq
 
 #calculate the normalized normal vector to the point boundary(t) using central difference formula for dydx
 def normal_line(s, boundary, eps = 1e-8):
@@ -26,6 +26,66 @@ def R(theta):
     return np.array([[np.cos(theta)**2 - np.sin(theta)**2, 2*np.cos(theta)*np.sin(theta)],
              [2*np.cos(theta)*np.sin(theta), np.sin(theta)**2 - np.cos(theta)**2]])
 
+# calculates distance between border and movement line for spot t on border
+def border_dist(t, m, a, b, border):
+    x, y = border(t)
+
+    # gets rid of infinite/ large slopes to solve some conditioning problems
+    if m > 1:
+        dist = (1/m)*(y-b) + a - x
+    else:
+        dist = (m*(x-a) + b) - y
+
+    return dist
+
+# given pos and vel info calculates the location of the next collision
+def collide(x, v, boundary, N=1000):
+
+    # np warning suppression
+    if v[0] == 0:
+        m = np.inf
+    else:
+        m = v[1]/v[0]
+
+    a = x[0]
+    b = x[1]
+
+    # uses gridsearch to find location of possible zeros
+    T = np.linspace(0,1,N)
+    obj_val = np.zeros(N)
+    for i in range(N):
+        obj_val[i] = border_dist(T[i], m, a, b, boundary)
+
+    sign_change = (obj_val[:-1] * obj_val[1:]) < 0
+    boundaries = np.where(sign_change==True)[0]
+
+    #number of possible solutions given the spacing of T
+    k = len(boundaries)
+
+    solns = np.zeros(k)
+    euc_dist = np.zeros(k)
+    positions = np.zeros([k, 2])
+
+    obj = lambda t: border_dist(t, m, a, b, boundary)
+
+    # using possible zeros finds every zero along the line and their distance to the starting position
+    for i in range(k):
+        bracket = [T[boundaries[i]], T[boundaries[i]+1]]
+        # bretnq should always converge like bisection.. if not change to bisect w/o much loss
+        solns[i] = brentq(obj, bracket[0], bracket[1], xtol = 1e-14)
+        positions[i] = boundary(solns[i])
+        euc_dist[i] = np.linalg.norm(positions[i]-x)
+
+    # masks out solutions along the correct movement direction (uses/requires constant velocity)
+    correct_dir = np.all(np.sign(positions - x) == np.sign(v), axis = 1)
+    euc_dist[~correct_dir] = np.inf
+
+    # t values with the closest minimum
+    final_t = solns[np.argmin(euc_dist)]
+    final_x = boundary(final_t)
+
+    return final_t, final_x
+
 #function for calculating one billiard bounce
 '''
 Takes in a current position, velocity, and problem-specific information and returns the next location and velocity
@@ -37,31 +97,31 @@ tol- tolerance for the minimization routine to take into account. Determined min
 poincare- boolean value for whether or not to return poincare section each bounce.
             if true returns: x_new, v_new, [poincare_section]
 '''
-def bounce(x, v, boundary, tol = 1e-8, poincare = False):
-    #normalize s.t. velocity is direction unit vector
-    v = v/np.linalg.norm(v)
-    #Solve system of parametrized equations to find where the ball hits the boundary
-    #Analytically solveable for some boundaries, but numerical solution works for any boundarie
-    #t,s = minimize(collide, x0 = (0.58, .5), bounds = ((.01,1), (0,1)), args = (x,v,boundary), tol = 1e-8, method = 'Powell').x
-    minargs = {'bounds': ((.01,1), (0,1)), 'args' : (x,v,boundary), 'tol': tol}
-    #basin hops to preform relative to tolerance needed
-    N = int((-np.log10(tol)))
-    t,s = basinhopping(collide, x0=(.5,.5), niter = N, minimizer_kwargs = minargs).x
-    m = boundary(s)
+def bounce(x, v, boundary, poincare = False):
+
+    t, m = collide(x, v, boundary)
+
     #finds normalized normal line to the boundary edge hit
-    n = normal_line(s, boundary)
+    n = normal_line(t, boundary)
+
     #finds vector between normal line and x-axis (using dot product)
     phi = np.arccos(np.abs(n[0]))
-    #corrects for QI and QIII
-    if m[0]/m[1] < 0:
-        phi = np.pi - phi
-    #new path from m
+    #corrects rotation matrix for QI and QIII
+
+    # np warning suppression
+    if m[1] != 0:
+        if m[0]/m[1] < 0:
+            phi = np.pi - phi
+
     v_new = -np.matmul(R(phi), v)
-    #print(m, v_new)
+
     #optional returning of poincare section
     if poincare:
+        #normalize s.t. velocity is direction unit vector
+        v = v/np.linalg.norm(v)
+
         v_tangent = np.sqrt(1 - np.dot(v, n)**2)
-        return m, v_new, np.array([s, v_tangent])
+        return m, v_new, np.array([t, v_tangent])
 
     return m, v_new
 
